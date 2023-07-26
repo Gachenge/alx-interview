f2d71d2fa4c1f25c46c2be8f6b4859c54d403ea2
Gachenge <gachenge1@gmail.com>
2023-07-26 18:57:14 +0300

    clean up

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,14 +4,13 @@ script to determine if a dataset is valid utf-8 encoding
 Return: True if data is a valid UTF-8 encoding, else return False
 A character in UTF-8 can be 1 to 4 bytes long
 """
-import codecs
 
 
 def validUTF8(data):
     """determine if the data is valid encoded"""
     try:
-        byte_data = bytes(data)
-        codecs.utf_8_decode(byte_data)
+        data = bytes(data)
+        data.decode('utf-8')
         return True
     except Exception as e:
         return False
