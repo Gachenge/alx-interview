72ef9d29c9d78b12df56fe970b4f42395d8e9404
Gachenge <gachenge1@gmail.com>
2023-08-11 16:38:22 +0300

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,22 +5,25 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
+function requet (lst, index) {
+  if (lst.length === index) {
+    return;
+  }
+  request(lst[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      requet(lst, index + 1);
+    }
+  });
+}
+
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const characters = JSON.parse(body).characters;
-
-    for (let i = 0; i < characters.length; i++) {
-      const characterUrl = characters[i];
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    }
+    const lst = JSON.parse(body).characters;
+    requet(lst, 0);
   }
 });
