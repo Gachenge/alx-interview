28545244ea3bf83fed5af99ff05987ebec8e2e94
Gachenge <gachenge1@gmail.com>
2023-08-29 16:36:25 +0300

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 
 def island_perimeter(grid):
-    """get the perimeter of an island. assume there are no holes"""
+    """
+    get the perimeter of an island. assume there are no holes
+    """
     if not grid:
         return 0
-    M, N = len(grid), len(grid[0])
+    row, col = len(grid), len(grid[0])
     perim = 0
 
-    for i in range(M):
-        for j in range(N):
+    assert(1 <= row and col <= 100)
+
+    for i in range(row):
+        for j in range(col):
             if grid[i][j] == 1:
                 if i == 0 or grid[i-1][j] == 0:
                     perim += 1
                 if j == 0 or grid[i][j-1] == 0:
                     perim += 1
-                if i == M-1 or grid[i+1][j] == 0:
+                if i == row-1 or grid[i+1][j] == 0:
                     perim += 1
-                if j == N-1 or grid[i][j+1] == 0:
+                if j == col-1 or grid[i][j+1] == 0:
                     perim += 1
 
     return perim
